dc83bbd3ce322430b66e6b1fb8d51cee305606e1
Furkan BASKAN <frknbskn@gmail.com>
2024-07-24 14:30:35 +0300

    handled simple middlewares.

--- usingAndCustMiddleware/Program.cs
+++ usingAndCustMiddleware/Program.cs
@@ -9,17 +9,43 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("=====");
+    await next(); //bir sonraki middleware'e geç.
+    await context.Response.WriteAsync("====");
+
+});
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync(">>>>");
+    await next(); //bir sonraki middleware'e geç.
+    await context.Response.WriteAsync("<<<<");
+
+});
+
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+
+//app.UseWelcomePage();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello world!");
+
+});
 
 app.Run();
